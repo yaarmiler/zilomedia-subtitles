73c0b5eea1c30f5a90293b258b823f5691d587f3
yaarmiler <yaarmiler20@gmail.com>
2025-06-29 19:18:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,56 @@
-// --- מערכים להפרדה לסרטים וסדרות לפי קטגוריה ---
-// מלא כאן את התכנים שלך:
-const dubbedMovies = [
+const db = window.db;
+const auth = window.auth;
 
+const dubbedMovies = [
+  {id:"madagascar3",title:"מדגסקר 3: מקרקסים את אירופה",year:2012,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F165628%2FMadagascar_3_Europe%2527s_Most_Wanted_2012Hebrew_Dubbed_Also_English%25D7%259E%25D7%2593%25D7%2592%25D7%25A1%25D7%25A7%25D7%2590%25D7%25A8.mp4%3Fhash%3DAgADVg",img:"https://i.imgur.com/0oJ8rDH.jpeg"},
+  {id:"robot_rose",title:"רוז הרובוטית",year:2024,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F165618%2F%25D7%25A8%25D7%2595%25D7%2596_%25D7%2594%25D7%25A8%25D7%2595%25D7%2591%25D7%2595%25D7%2598%25D7%2599%25D7%25AA_2024_The_Wild_Robot_%25D7%259E%25D7%2593%25D7%2595%25D7%2591%25D7%2591_%25D7%2594%25D7%2595%25D7%25A2%25D7%259C%25D7%2594_%25D7%25A2%25D7%2599_%25F0%259D%2590%2581%25F0%259D%2590%258E.mp4%3Fhash%3DAgADrx",img:"https://i.imgur.com/lvGeaK6.jpeg"},
+  {id:"the_spongebob_squarepants_movie",title:"בובספוג: הסרט",year:2004,src:"https://dl.dropboxusercontent.com/scl/fi/p56jdckyn522nj8dva4sm/_-_-_-_2004_.mp4?rlkey=6vihdhqghftrpu142mqeriagy&st=t7hsu9sw",img:"https://i.imgur.com/0rjGCU0.png"},
+  {id:"big_hero_6",title:"6 גיבורים",year:2014,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F186137%2F6%2B%25D7%2592%25D7%2599%25D7%2591%25D7%2595%25D7%25A8%25D7%2599%25D7%259D%2B2014%2B%25D7%259E%25D7%2593%25D7%2595%25D7%2591%25D7%2591-Segment%2B1.mp4%3Fhash%3DAgAD_w",img:"https://i.imgur.com/rqrqITT.jpeg"}
 ];
+
 const subtitledMovies = [
-  // ...
+  {id:"yes_lah_bitzim",title:"יש לה ביצים",year:2006,src:"https://tj.tbnbotsnetwork.workers.dev/165634/1080p+BluRay+2006+%D7%96%D7%99%D7%A8%D7%94+%D7%9E%D7%93%D7%99%D7%94+%D7%99%D7%A9+%D7%9C%D7%94+%D7%91%D7%99%D7%A6%D7%99%D7%9D.mp4?hash=AgADdB",img:"https://i.imgur.com/EiccKVQ.jpeg"},
+  {id:"kara_robinson_story",title:"הילדה שנמלטה: הסיפור של קארה רובינסון",year:2023,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F165638%2F%25D7%2594%25D7%2599%25D7%259C%25D7%2593%25D7%2594_%25D7%25A9%25D7%25A0%25D7%259E%25D7%259C%25D7%2598%25D7%2594_%25D7%2594%25D7%25A1%25D7%2599%25D7%25A4%25D7%2595%25D7%25A8_%25D7%25A9%25D7%259C_%25D7%25A7%25D7%2590%25D7%25A8%25D7%2594_%25D7%25A8%25D7%2595%25D7%2591%25D7%2599%25D7%25A0%25D7%25A1%25D7%2595%25D7%259F_2023.mp4%3Fhash%3DAgADTx",img:"https://i.imgur.com/tANPbX3.jpeg"},
+  {id:"flow",title:"עם הזרם",year:2024,src:"https://xstreamx.films365.org/movie/da067ce8-a5dd-4965-85d8-e2e86f747a10/Flow",img:"https://i.imgur.com/PbXM7QV.jpeg"},
+  {id:"a_minecraft_movie",title:"מיינקראפט: הסרט",year:2025,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F165648%2F%25D7%259E%25D7%2599%25D7%2599%25D7%25A0%25D7%25A7%25D7%25A8%25D7%2590%25D7%25A4%25D7%2598_%25D7%2594%25D7%25A1%25D7%25A8%25D7%2598_2025_%25D7%25AA%25D7%25A8%25D7%2592%25D7%2595%25D7%259D_%25D7%259E%25D7%2595%25D7%2591%25D7%25A0%25D7%2594_1080p_WEB_DL_%25D7%2594%25D7%2595%25D7%25A2%25D7%259C%25D7%2594_%25D7%25A2%2522%25D7%2599_%25F0%259D%2590%2581%25F0%259D%2590%258E%25F0%259D%2590%2592%25F0%259D%2590%2592.mp4%3Fhash%3DAgADmh",img:"https://i.imgur.com/7UVvdLg.jpeg"},
+  {id:"a_good_day_to_die_hard",title:"מת לחיות ביום טוב",year:2013,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F165659%2F1080p%2BBluRay%2B2013%2B%25D7%2596.%25D7%259E%2B%25D7%259E%25D7%25AA%2B%25D7%259C%25D7%2597%25D7%2599%25D7%2595%25D7%25AA%2B%25D7%2591%25D7%2599%25D7%2595%25D7%259D%2B%25D7%2598%25D7%2595%25D7%2591.mp4%3Fhash%3DAgADeQ",img:"https://i.imgur.com/Pqwlb3W.jpeg"},
+  {id:"snow_white",title:"שלגיה",year:2025,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F165623%2F%25D7%25A9%25D7%259C%25D7%2592%25D7%2599%25D7%2594_2025_Snow_White_1080p_WEB_DL_%25D7%2594%25D7%2595%25D7%25A2%25D7%259C%25D7%2594_%25D7%25A2%25D7%2599_%25F0%259D%2590%2581%25F0%259D%2590%258E%25F0%259D%2590%2592%25F0%259D%2590%2592.mp4%3Fhash%3DAgADBh",img:"https://i.imgur.com/qhkzqtv.jpeg"},
+  {id:"wicked",title:"מרשעת",year:2024,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F165672%2F%25D7%259E%25D7%25A8%25D7%25A9%25D7%25A2%25D7%25AA_2024_Wicked_%25D7%2594%25D7%2595%25D7%25A2%25D7%259C%25D7%2594_%25D7%25A2%2522%25D7%2599_%25F0%259D%2590%2581%25F0%259D%2590%258E%25F0%259D%2590%2592%25F0%259D%2590%2592.mp4%3Fhash%3DAgADvR",img:"https://i.imgur.com/NoCQY3d.jpeg"},
+  {id:"eternal_sunshine_of_the_spotless_mind",title:"שמש נצחית בראש צלול",year:2004,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F187999%2F1080p%2BBluRay%2B2004%2B%25D7%25A9%25D7%259E%25D7%25A9%2B%25D7%25A0%25D7%25A6%25D7%2597%25D7%2599%25D7%25AA%2B%25D7%2591%25D7%25A8%25D7%2590%25D7%25A9%2B%25D7%25A6%25D7%259C%25D7%2595%25D7%259C.mp4%3Fhash%3DAgADzw",img:"https://i.imgur.com/UyIx6WU.jpeg"},
+  {id:"joker",title:"ג'וקר",year:2019,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F165626%2F1080p%2BBluRay%2B2019%2B%25D7%2596%25D7%2599%25D7%25A8%25D7%2594%2B%25D7%259E%25D7%2593%25D7%2599%25D7%2594%2B%25D7%2592%2527%25D7%2595%25D7%25A7%25D7%25A8.mp4%3Fhash%3DAgADDA",img:"https://i.imgur.com/q390vHH.jpeg"},
+  {id:"coraline",title:"קורליין ודלת הקסמים",year:2009,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F165692%2FCoraline.2009.1080p.BluRay.x264.AAC5.1-%255BYTS.MX%255D.mp4%3Fhash%3DAgADMx",img:"https://i.imgur.com/DcIuBV9.jpeg"},
+  {id:"harry_potter_and_the_philosopher's_stone",title:"הארי פוטר ואבן החכמים",year:2001,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F162615%2F1080p%2BBluRay%2B2001%2B%25D7%2596%25D7%2599%25D7%25A8%25D7%2594%2B%25D7%259E%25D7%2593%25D7%2599%25D7%2594%2B%25D7%2594%25D7%2590%25D7%25A8%25D7%2599%2B%25D7%25A4%25D7%2595%25D7%2598%25D7%25A8%2B1.mp4%3Fhash%3DAgADPQ",img:"https://i.imgur.com/ahNuo3q.jpeg"},
+  {id:"twilight",title:"דמדומים",year:2008,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F168120%2F1080p%2BBluRay%2B2008%2B%25D7%2596%25D7%2599%25D7%25A8%25D7%2594%2B%25D7%259E%25D7%2593%25D7%2599%25D7%2594%2B%25D7%2593%25D7%259E%25D7%2593%25D7%2595%25D7%259E%25D7%2599%25D7%259D.mp4%3Fhash%3DAgAD7A",img:"https://i.imgur.com/fV3ekwT.jpeg"},
+  {id:"the_twilight_saga:_new_moon",title:"דמדומים 2: ירח חדש",year:2009,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F167980%2F1080p%2BBluRay%2B2009%2B%25D7%2596%25D7%2599%25D7%25A8%25D7%2594%2B%25D7%259E%25D7%2593%25D7%2599%25D7%2594%2B%25D7%2593%25D7%259E%25D7%2593%25D7%2595%25D7%259E%25D7%2599%25D7%259D%2B2.mp4%3Fhash%3DAgADtw",img:"https://i.imgur.com/KKN6x2i.jpeg"},
+  {id:"the_twilight_saga:_eclipse",title:"דמדומים 3: ליקוי חמה",year:2010,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F168004%2F1080p%2BBluRay%2B2010%2B%25D7%2596%25D7%2599%25D7%25A8%25D7%2594%2B%25D7%259E%25D7%2593%25D7%2599%25D7%2594%2B%25D7%2593%25D7%259E%25D7%2593%25D7%2595%25D7%259E%25D7%2599%25D7%259D%2B3.mp4%3Fhash%3DAgADyg",img:"https://i.imgur.com/fvB40Oi.jpeg"},
+  {id:"the_twilight_saga:_breaking_dawn",title:"דמדומים 4: שחר מפציע - חלק 1",year:2011,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F168009%2F1080p%2BBluRay%2B2011%2B%25D7%2596.%25D7%259E%2B%25D7%2593%25D7%259E%25D7%2593%25D7%2595%25D7%259E%25D7%2599%25D7%259D%2B4%2B%25D7%2597%25D7%259C%25D7%25A7%2B1.mp4%3Fhash%3DAgADzA",img:"https://i.imgur.com/uENbjgk.jpeg"},
+  {id:"the_twilight_saga:_breaking_dawn_part_2",title:"דמדומים 4: שחר מפציע - חלק 2",year:2012,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F168012%2F1080p%2BBluRay%2B2012%2B%25D7%2596.%25D7%259E%2B%25D7%2593%25D7%259E%25D7%2593%25D7%2595%25D7%259E%25D7%2599%25D7%259D%2B4%2B%25D7%2597%25D7%259C%25D7%25A7%2B2.mp4%3Fhash%3DAgAD1Q",img:"https://i.imgur.com/92hguFz.jpeg"},
+  {id:"harry_potter_and_the_chamber_of_secrets",title:"הארי פוטר וחדר הסודות",year:2002,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F163371%2F1080p%2BBluRay%2B2002%2B%25D7%2596%25D7%2599%25D7%25A8%25D7%2594%2B%25D7%259E%25D7%2593%25D7%2599%25D7%2594%2B%25D7%2594%25D7%2590%25D7%25A8%25D7%2599%2B%25D7%25A4%25D7%2595%25D7%2598%25D7%25A8%2B2.mp4%3Fhash%3DAgADPg",img:"https://i.imgur.com/jA7SGNn.png"},
+  {id:"harry_potter_and_the_prisoner_of_azkaban",title:"הארי פוטר והאסיר מאזקבאן",year:2004,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F163418%2F1080p%2BBluRay%2B2004%2B%25D7%2596%25D7%2599%25D7%25A8%25D7%2594%2B%25D7%259E%25D7%2593%25D7%2599%25D7%2594%2B%25D7%2594%25D7%2590%25D7%25A8%25D7%2599%2B%25D7%25A4%25D7%2595%25D7%2598%25D7%25A8%2B3.mp4%3Fhash%3DAgADPw",img:"https://i.imgur.com/QueMvLs.png"},
+  {id:"harry_potter_and_the_goblet_of_fire",title:"הארי פוטר וגביע האש",year:2005,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F163453%2F1080p%2BBluRay%2B2005%2B%25D7%2596%25D7%2599%25D7%25A8%25D7%2594%2B%25D7%259E%25D7%2593%25D7%2599%25D7%2594%2B%25D7%2594%25D7%2590%25D7%25A8%25D7%2599%2B%25D7%25A4%25D7%2595%25D7%2598%25D7%25A8%2B4.mp4%3Fhash%3DAgADQA",img:"https://i.imgur.com/DP4Pwti.png"},
+  {id:"harry_potter_and_the_order_of_the_phoenix",title:"הארי פוטר ומסדר עוף החול",year:2007,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F163470%2F1080p%2BBluRay%2B2007%2B%25D7%2596%25D7%2599%25D7%25A8%25D7%2594%2B%25D7%259E%25D7%2593%25D7%2599%25D7%2594%2B%25D7%2594%25D7%2590%25D7%25A8%25D7%2599%2B%25D7%25A4%25D7%2595%25D7%2598%25D7%25A8%2B5.mp4%3Fhash%3DAgADQQ",img:"https://i.imgur.com/4oNTsO5.jpeg"},
+  {id:"harry_potter_and_the_half_blood_prince",title:"הארי פוטר והנסיך חצוי-הדם",year:2009,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F163510%2F1080p%2BBluRay%2B2009%2B%25D7%2596%25D7%2599%25D7%25A8%25D7%2594%2B%25D7%259E%25D7%2593%25D7%2599%25D7%2594%2B%25D7%2594%25D7%2590%25D7%25A8%25D7%2599%2B%25D7%25A4%25D7%2595%25D7%2598%25D7%25A8%2B6.mp4%3Fhash%3DAgADQg",img:"https://i.imgur.com/ddKYQQQ.jpeg"},
+  {id:"harry_potter_and_the_deathly_hallows_part_1",title:"הארי פוטר ואוצרות המוות - חלק 1",year:2010,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F163538%2F1080p%2BBluRay%2B2010%2B%25D7%2596%25D7%2599%25D7%25A8%25D7%2594%2B%25D7%259E%25D7%2593%25D7%2599%25D7%2594%2B%25D7%2594%25D7%2590%25D7%25A8%25D7%2599%2B%25D7%25A4%25D7%2595%25D7%2598%25D7%25A8%2B7%2B%25D7%25971.mp4%3Fhash%3DAgADQw",img:"https://i.imgur.com/BwjwVcM.jpeg"},
+  {id:"harry_potter_and_the_deathly_hallows_part_2",title:"הארי פוטר ואוצרות המוות - חלק 2",year:2011,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F163558%2F1080p%2BBluRay%2B2011%2B%25D7%2596%25D7%2599%25D7%25A8%25D7%2594%2B%25D7%259E%25D7%2593%25D7%2599%25D7%2594%2B%25D7%2594%25D7%2590%25D7%25A8%25D7%2599%2B%25D7%25A4%25D7%2595%25D7%2598%25D7%25A8%2B7%2B%25D7%25972.mp4%3Fhash%3DAgADRA",img:"https://i.imgur.com/vDpaaTG.jpeg"},
+  {id:"the_notebook",title:"היומן",year:2004,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F163576%2F1080p%2BBluRay%2B2004%2B%25D7%2596%25D7%2599%25D7%25A8%25D7%2594%2B%25D7%259E%25D7%2593%25D7%2599%25D7%2594%2B%25D7%2594%25D7%2599%25D7%2595%25D7%259E%25D7%259F.mp4%3Fhash%3DAgADeR",img:"https://i.imgur.com/2duKzlH.jpeg"},
+  {id:"the_flash",title:"הפלאש",year:2023,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F163634%2F1080p%2BBluRay%2B2023%2B%25D7%2596%25D7%2599%25D7%25A8%25D7%2594%2B%25D7%259E%25D7%2593%25D7%2599%25D7%2594%2B%25D7%2594%25D7%25A4%25D7%259C%25D7%2590%25D7%25A9.mp4%3Fhash%3DAgADdh",img:"https://i.imgur.com/BBxGlky.jpeg"},
+  {id:"men_in_black_ii",title:"גברים בשחור 2",year:2002,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F163757%2F1080p%2BBluRay%2B2002%2B%25D7%2596%25D7%2599%25D7%25A8%25D7%2594%2B%25D7%259E%25D7%2593%25D7%2599%25D7%2594%2B%25D7%2592%25D7%2591%25D7%25A8%25D7%2599%25D7%259D%2B%25D7%2591%25D7%25A9%25D7%2597%25D7%2595%25D7%25A8%2B2.mp4%3Fhash%3DAgADZA",img:"https://i.imgur.com/SBef41T.jpeg"},
+  {id:"charlie's_angels",title:"המלאכיות של צ'רלי",year:2000,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F163764%2F1080p%2BBluRay%2B2000%2B%25D7%2596.%25D7%259E%2B%25D7%2594%25D7%259E%25D7%259C%25D7%2590%25D7%259B%25D7%2599%25D7%2595%25D7%25AA%2B%25D7%25A9%25D7%259C%2B%25D7%25A6%2527%25D7%25A8%25D7%259C%25D7%2599.mp4%3Fhash%3DAgADvw",img:"https://i.imgur.com/sTiTYFI.jpeg"},
+  {id:"charlie's_angels:_full_throttle",title:"המלאכיות של צ'רלי: בהילוך גבוה",year:2003,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F163777%2F%25D7%2594%25D7%259E%25D7%259C%25D7%2590%25D7%259B%25D7%2599%25D7%2595%25D7%25AA_%25D7%25A9%25D7%259C_%25D7%25A6%2527%25D7%2590%25D7%25A8%25D7%259C%25D7%2599_%25D7%2591%25D7%2594%25D7%2599%25D7%259C%25D7%2595%25D7%259A_%25D7%2592%25D7%2591%25D7%2595%25D7%2594_2003.mp4%3Fhash%3DAgADUg",img:"https://i.imgur.com/zEciMnc.jpeg"},
+  {id:"charlie's_angels",title:"המלאכיות של צ'רלי",year:2019,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F163788%2F1080p%2BBluRay%2B2019%2B%25D7%2596.%25D7%259E%2B%25D7%2594%25D7%259E%25D7%259C%25D7%2590%25D7%259B%25D7%2599%25D7%2595%25D7%25AA%2B%25D7%25A9%25D7%259C%2B%25D7%25A6%2527%25D7%25A8%25D7%259C%25D7%2599.mp4%3Fhash%3DAgADXg",img:"https://i.imgur.com/Mz8duur.jpeg"},
+  {id:"thunderbolts*",title:"ת'אנדרבולטס*",year:2025,src:"https://xstreamx.films365.org/movie/43c9266f-11da-4fd6-be37-9d6d299646a9/Thunderbolts*",img:"https://i.imgur.com/X84xvHT.jpeg",subtitles: [
+      { label:"עברית",src:"/subs/Thunderbolts*/Hebrew.vtt",lang:"he"},
+      { label:"אנגלית",src:"/subs/Thunderbolts*/English.vtt",lang:"en"}
+    ]
+  },
+   {id:"sinners",title:"חוטאים",year:2025,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F167189%2F%25D7%2597%25D7%2595%25D7%2598%25D7%2590%25D7%2599%25D7%259D%2B2025%2B1080p%2BWEB-DL%2B%25F0%259D%2590%2581%25F0%259D%2590%258E%25F0%259D%2590%2592%25F0%259D%2590%2592.mp4%3Fhash%3DAgADWR",img:"https://i.imgur.com/qdbYgzM.jpeg"},
+  {id:"me_before_you",title:"ללכת בדרכך",year:2016,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F172973%2F1080p%2BBluRay%2B2016%2B%25D7%2596%25D7%2599%25D7%25A8%25D7%2594%2B%25D7%259E%25D7%2593%25D7%2599%25D7%2594%2B%25D7%259C%25D7%259C%25D7%259B%25D7%25AA%2B%25D7%2591%25D7%2593%25D7%25A8%25D7%259B%25D7%259A.mp4%3Fhash%3DAgAD3w",img:"https://i.imgur.com/vfdhodJ.jpeg"}
 ];
+
 const israeliMovies = [
-  // ...
+  {id:"full_speed",title:"פול ספיד",year:2021,src:"https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F165528%2F1080p%2BWEB-DL%2B2021%2B%25D7%2596%25D7%2599%25D7%25A8%25D7%2594%2B%25D7%259E%25D7%2593%25D7%2599%25D7%2594%2B%25D7%25A4%25D7%2595%25D7%259C%2B%25D7%25A1%25D7%25A4%25D7%2599%25D7%2593.mp4%3Fhash%3DAgADqR",img:"https://i.imgur.com/jXQKZmw.jpeg"}
 ];
+
 // סדרות
 const dubbedSeries = [
 id: "jurassic_world_camp_cretaceous",
@@ -78,12 +120,37 @@ id: "jurassic_world_camp_cretaceous",
    ]
  }
 ];
+
 const subtitledSeries = [
-  // ...
+  {
+    id: "ahsoka",
+    title: "אסוקה",
+    year: 2023,
+    img: "https://i.imgur.com/K33GYDG.jpeg",
+    episodesTotal: 8,
+    isSeries: true,
+    seasons: [
+      {
+        name: "עונה 1",
+        episodes: [
+          {title: "פרק 1", src: "https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F193404%2F%25D7%2590%25D7%25A1%25D7%2595%25D7%25A7%25D7%2594%2B%25D7%25A21%2B%25D7%25A41%2B1080P%2B%25D7%25A2%2522%25D7%2599%2B%25D7%2599%25D7%2594%25D7%2595%25D7%2593%25D7%2594%2B%25D7%2591%25D7%259F%2B%25D7%2592%25D7%2591%25D7%2599%25D7%25A8.mp4%3Fhash%3DAgADfQ"},
+          {title: "פרק 2", src: "https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F193391%2F%25D7%2590%25D7%25A1%25D7%2595%25D7%25A7%25D7%2594%2B%25D7%25A21%2B%25D7%25A42%2B1080P%2B%25D7%25A2%2522%25D7%2599%2B%25D7%2599%25D7%2594%25D7%2595%25D7%2593%25D7%2594%2B%25D7%2591%25D7%259F%2B%25D7%2592%25D7%2591%25D7%2599%25D7%25A8.mp4%3Fhash%3DAgADmQ"},
+          {title: "פרק 3", src: "https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F193399%2F%25D7%2590%25D7%25A1%25D7%2595%25D7%25A7%25D7%2594_%25D7%25A21_%25D7%25A43_1080P_%25D7%25A2%2522%25D7%2599_%25D7%2599%25D7%2594%25D7%2595%25D7%2593%25D7%2594_%25D7%2591%25D7%259F_%25D7%2592%25D7%2591%25D7%2599%25D7%25A8_.mp4%3Fhash%3DAgADzx"},
+          {title: "פרק 4", src: "https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F193392%2F%25D7%2590%25D7%25A1%25D7%2595%25D7%25A7%25D7%2594%2B%25D7%25A21%2B%25D7%25A44%2B1080P%2B%25D7%25A2%2522%25D7%2599%2B%25D7%2599%25D7%2594%25D7%2595%25D7%2593%25D7%2594%2B%25D7%2591%25D7%259F%2B%25D7%2592%25D7%2591%25D7%2599%25D7%25A8.mp4%3Fhash%3DAgAD5w"},
+          {title: "פרק 5", src: "https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F193395%2F%25D7%2590%25D7%25A1%25D7%2595%25D7%25A7%25D7%2594%2B%25D7%25A21%2B%25D7%25A45%2B1080P%2B%25D7%25A2%2522%25D7%2599%2B%25D7%2599%25D7%2594%25D7%2595%25D7%2593%25D7%2594%2B%25D7%2591%25D7%259F%2B%25D7%2592%25D7%2591%25D7%2599%25D7%25A8.mp4%3Fhash%3DAgADIh"},
+          {title: "פרק 6", src: "https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F193396%2F%25D7%2590%25D7%25A1%25D7%2595%25D7%25A7%25D7%2594%2B%25D7%25A21%2B%25D7%25A46%2B1080P%2B%25D7%25A2%2522%25D7%2599%2B%25D7%2599%25D7%2594%25D7%2595%25D7%2593%25D7%2594%2B%25D7%2591%25D7%259F%2B%25D7%2592%25D7%2591%25D7%2599%25D7%25A8.mp4%3Fhash%3DAgADJR"},
+          {title: "פרק 7", src: "https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F193400%2F%25D7%2590%25D7%25A1%25D7%2595%25D7%25A7%25D7%2594%2B%25D7%25A21%2B%25D7%25A47%2B1080P%2B%25D7%25A2%2522%25D7%2599%2B%25D7%2599%25D7%2594%25D7%2595%25D7%2593%25D7%2594%2B%25D7%2591%25D7%259F%2B%25D7%2592%25D7%2591%25D7%2599%25D7%25A8.mp4%3Fhash%3DAgADjh"},
+          {title: "פרק 8", src: "https://zilomedia.yaarmiler20.workers.dev/?url=https%3A%2F%2Ftj.tbnbotsnetwork.workers.dev%2F193402%2F%25D7%2590%25D7%25A1%25D7%2595%25D7%25A7%25D7%2594%2B%25D7%25A21%2B%25D7%25A48%2B1080P%2B%25D7%25A2%2522%25D7%2599%2B%25D7%2599%25D7%2594%25D7%2595%25D7%2593%25D7%2594%2B%25D7%2591%25D7%259F%2B%25D7%2592%25D7%2591%25D7%2599%25D7%25A8.mp4%3Fhash%3DAgADsA"}
+        ]
+      }
+    ]
+  }
 ];
+
 const israeliSeries = [
-  // ...
+
 ];
+
 // ערוצים חיים
 const channels = [
   // דוג': {title:"ערוץ 12", img:"...", src:"..."}
